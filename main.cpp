93bfa4eb5850fbc7250358e23d80ca9a98028d5f
Dragonix22 <98195799+Dragonix22@users.noreply.github.com>
2026-06-27 12:59:49 -0400

    Improved error handling

--- main.cpp
+++ main.cpp
@@ -2,30 +2,35 @@
 #include <stack>
 #include <string>
 #include <unordered_map>
-int main(){
-    std::cout << "Enter a math expression: ";
-    std::string expr;
-    std::cin >> expr;
 
+//stores all operators with value equal to level of precedence(eg - and + have precedence 1, * and / have precedence 2, etc)
+const std::unordered_map<char,int> precedence={
+    {'+',1},{'-',1},{'*',2},{'/',2}
+};
+
+bool isDigit(char c){
+    return (c>='0'&&c<='9');
+}
+
+bool isOperator(char c){
+    return precedence.count(c);
+}
+
+std::string infixToPostfix(const std::string& expr){
     std::stack<char> operator_stack;//only should have operators and left parenthesis
-    std::string postfix="";
-    //stores all operators with value equal to level of precedence(eg - and + have precedence 1, * and / have precedence 2, etc)
-    std::unordered_map<char,int> operators={
-        {'+',1},{'-',1},{'*',2},{'/',2}
-    };
+    std::string postfix="";    
     //Shunting Yard Algorithm(infix to postfix conversion)
     for(int i=0;i<expr.size();i++){
         char c =expr[i];
-        if(c>='0'&&c<='9'){
+        if(isDigit(c)){
             //single digits only for now
             postfix+=c;
-            //std::cout<<i<<" digit parsed \n";
         }
         else if(c=='('){
             operator_stack.push(c);
         }
         else if(c==')'){
-            while(operator_stack.top()!='('){
+            while(!operator_stack.empty()&&operator_stack.top()!='('){
                 postfix+=operator_stack.top();
                 operator_stack.pop();
             }
@@ -34,27 +39,78 @@ int main(){
             }
             operator_stack.pop();
         }
-        else if(operators[c]>=1){
+        else if(isOperator(c)){
             //if its + or - all operators of stack are popped, if * or / only pop those ones            
-            while(!operator_stack.empty()&&c!='('&&operators[operator_stack.top()]>=operators[c]){
+            while(!operator_stack.empty()&&operator_stack.top()!='('&&precedence.at(operator_stack.top())>=precedence.at(c)){
                 char top = operator_stack.top();
                 postfix+=top;                        
                 operator_stack.pop();
             }
             operator_stack.push(c);
-            //std::cout<<i<<" operator parsed \n";
         }
         else{
             throw std::runtime_error("Invalid character");
         }
     } 
     while(!operator_stack.empty()){
+        if(operator_stack.top()=='('){
+            throw std::runtime_error("Mismatched parentheses");
+        }
         postfix+=operator_stack.top();
         operator_stack.pop();
     }
-    std::cout<<"Postfix: "<< postfix << std::endl;
-    //postfix evaluation
+    return postfix;
+}
+
+int applyOperator(int lhs, int rhs, char op)
+{
+    switch(op)
+    {
+        case '+': return lhs + rhs;
+        case '-': return lhs - rhs;
+        case '*': return lhs * rhs;
+        case '/': return lhs / rhs;
+        default:
+            throw std::runtime_error("Unknown operator");
+    }
+}
+
+int evaluatePostfix(const std::string& postfix){
+    std::stack<int> values;
+    for(size_t i=0;i<postfix.size();i++){
+        char c = postfix[i];
+        if(isDigit(c)){
+            values.push(c-'0');
+        }
+        else{
+            if(values.size()<2){
+                throw std::runtime_error("Incorrect Postfix Expression");
+            }
+            int rhs=values.top();
+            values.pop();
+            int lhs = values.top();
+            values.pop();
+            
+            values.push(applyOperator(lhs,rhs,c));
+            
+        }
+    }
+    if (values.size() != 1) {
+        throw std::runtime_error("Invalid postfix expression");
+    }
+    return values.top();
+}
+
+int main(){
+    std::cout << "Enter a math expression: ";
+    std::string expr;
+    std::cin >> expr;
     
+    std::string postfix=infixToPostfix(expr);   
+    std::cout<<"Postfix: "<< postfix << std::endl;
+
+    int answer = evaluatePostfix(postfix);
+    std::cout<<"Answer: "<<answer<<std::endl;
 
     return(0);
 }
